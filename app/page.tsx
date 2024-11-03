6278b4b06221001e8045f0c8432891e6e8b62508
harry1988123 <harendra.singh2015@vit.ac.in>
2024-11-03 15:23:48 +0530

    Top level

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import SearchBar from "./components/search-bar";
 import FilterChips from "./components/filter-chips";
 import PokemonList from "./components/pokemon-list";
+import { Suspense } from "react";
+import React from "react";
 
 export default function Home() {
   return (
+    <Suspense>
     <main className="min-h-screen bg-background">
       <div className="max-w-7xl mx-auto px-4 py-8">
         <header className="mb-8 text-center">
@@ -14,5 +17,6 @@ export default function Home() {
         <PokemonList />
       </div>
     </main>
+    </Suspense>
   );
 }
